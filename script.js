a17e8e3c0879b5a1e1a151cd61475dfb6f021fde
Tahircyber <Tahirali2602@outlook.com>
2025-02-20 06:06:14 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const currentPage = window.location.pathname.split("/").pop(); // Get the current file name
+        document.querySelectorAll("nav a").forEach(link => {
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active");
+            }
+        });
+    });
